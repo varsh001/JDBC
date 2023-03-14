9510e5329e112ff41738c4ca80822f3e3e740142
hp <hp@DESKTOP-1E2TM25>
2023-03-14 13:45:27 +0530

    update code

--- JDBC/src/main/java/jdbc/Array1.java
+++ JDBC/src/main/java/jdbc/Array1.java
@@ -3,10 +3,19 @@ package jdbc;
 public class Array1 {
 
 	public static void main(String[] args) {
-		System.out.println("Start");
-	   System.out.println("Hii");
+	   System.out.println("Start");
+	   {
+		   for (int i=1;i<=3;i++)
+		   {
+			   for (int j=3;j>=i;j--)
+			   {
+			   System.out.print("*");
+			   }
+			   System.out.println();
+		   }
 	   System.out.println("Stop");
-
+	   }
 	}
-
 }
+
+
