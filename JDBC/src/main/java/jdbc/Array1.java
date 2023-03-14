459f0104573c26ac97d1d70f0e2813796635238a
hp <hp@DESKTOP-1E2TM25>
2023-03-14 13:34:16 +0530

    new code

--- JDBC/src/main/java/jdbc/Array1.java
+++ JDBC/src/main/java/jdbc/Array1.java
@@ -3,7 +3,9 @@ package jdbc;
 public class Array1 {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 	   System.out.println("Hii");
+	   System.out.println("Stop");
 
 	}
 
